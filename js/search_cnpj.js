5061b8c32fa753a064c9d29da46c80869528af39
Antonia Luciana Pires <antonialucianapires@gmail.com>
2023-12-06 19:34:17 -0300

    feat: integração api + ajuste layout

---
+++ js/search_cnpj.js
@@ -0,0 +1,46 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+  const cnpj = document.getElementById('cnpjInput').value;
+  searchCnpj(cnpj);
+});
+
+function searchCnpj(cnpj) {
+  fetch(`https://api-publica.speedio.com.br/buscarcnpj?cnpj=${cnpj}`)
+    .then(response => response.json())
+    .then(data => {
+      displayCompanyInfo(data);
+    })
+    .catch(() => {
+      displayNotFound();
+    });
+}
+
+function displayCompanyInfo(data) {
+  const container = document.getElementById('searchResults');
+  container.innerHTML = ''; // Limpa resultados anteriores
+
+  if (data && data['RAZAO SOCIAL']) {
+    const infoHtml = `
+      <section class="company-info" aria-labelledby="companyName" role="complementary">
+        <h2 id="companyName">${data['RAZAO SOCIAL']} - ${data['NOME FANTASIA']}</h2>
+        <dl class="company-details">
+          <dt>Razão Social:</dt>
+          <dd>${data['RAZAO SOCIAL']}</dd>
+          <dt>Nome Fantasia:</dt>
+          <dd>${data['NOME FANTASIA']}</dd>
+          <dt>CNPJ:</dt>
+          <dd>${data['CNPJ']}</dd>
+          <dt>Status:</dt>
+          <dd>${data['STATUS']}</dd>
+          <!-- Outros campos da API -->
+        </dl>
+      </section>
+    `;
+    container.innerHTML = infoHtml;
+  } else {
+    displayNotFound();
+  }
+}
+
+function displayNotFound() {
+  document.getElementById('searchResults').innerHTML = '<div>CNPJ não encontrado</div>';
+}
